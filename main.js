4a95cfed2a4a7d291f0aa79c58f0a1f9a831481f
Valerio <valerioageno@yahoo.it>
2020-08-28 23:24:15 +0200

    remove cursor bug

--- main.js
+++ main.js
@@ -1,18 +1,82 @@
-//initialize default functions
-const initImageCursor = new Cursor();
-const readyHome = new initHome();
 const initList = new listItemsShow();
+function listItemsShow(){
+    let items = document.querySelectorAll('.list-item');
+    let links = document.querySelectorAll('a');
+    let images = document.querySelectorAll('.image-container');
 
-//function used in barba lifecycle to control the effects durations
-function delay(n) {
-    n = n || 2000;
-    return new Promise((done) => {
-        setTimeout(() => {
-            done();
-        }, n);
-    });
+    function showImages(){
+        switch (this.value) {
+            case 0:
+                images[0].classList.add("img-reveal");
+                links[0].style.color = "white";
+                links[0].classList.add('right-margin');
+                break;
+            case 1:
+                images[1].classList.add("img-reveal");
+                links[1].style.color = "white";
+                links[1].classList.add('right-margin');
+                break;
+
+            case 2:
+                images[2].classList.add("img-reveal");
+                links[2].style.color = "white";
+                links[2].classList.add('right-margin');
+                break;
+            case 3:
+                images[3].classList.add("img-reveal");
+                links[3].style.color = "white";
+                links[3].classList.add('right-margin');
+                break;
+        }
+    }
+    
+    function removeImages(){
+        switch (this.value) {
+            case 0:
+                images[0].classList.remove("img-reveal");
+                links[0].style.color = "black";
+                links[0].classList.remove('right-margin');
+                break;
+            case 1:
+                images[1].classList.remove("img-reveal");
+                links[1].style.color = "black";
+                links[1].classList.remove('right-margin');
+                break;
+            case 2:
+                images[2].classList.remove("img-reveal");
+                links[2].style.color = "black";
+                links[2].classList.remove('right-margin');
+                break;
+            case 3:
+                images[3].classList.remove("img-reveal");
+                links[3].style.color = "black";
+                links[3].classList.remove('right-margin');
+                break;
+        }
+    }
+    //home list show cursor image effects
+    for (let i = 0; i < items.length; i++) {     
+        items[i].addEventListener('mouseover', showImages)
+        items[i].addEventListener("mouseleave", removeImages) 
+
+    }
 }
 
+//initialize image cursor effect
+function moveCursor(e) {
+    for (let i = 0; i < document.querySelectorAll('.mouse').length; i++) {
+        gsap.to(document.querySelectorAll('.mouse')[i], 0.23, {
+            left: e.pageX - 200,
+            top: e.pageY -300,
+            ease: Power4.easOut
+        });
+    } 
+}
+
+document.addEventListener('mousemove', moveCursor);
+
+//home list show effect
+gsap.from('li.list-item a', {y: 170, duration: .8, display: 'block', stagger: 0.2, ease: "power2.out" })
 
 //barba lifecycle
 barba.init({
@@ -22,12 +86,14 @@ barba.init({
         name: 'to-pages',
         async leave(data){
             const done = this.async();
+            document.removeEventListener("mousemove", moveCursor);
             pageTransitionToPage(data.next.namespace);
             await delay(1300);
             done();
         },
         async enter() {
             contentAnimationToPage();
+            document.addEventListener('mousemove', moveCursor);
         },
         from: {
             namespace: 'home',
@@ -52,9 +118,8 @@ barba.init({
         async enter(data) {
             contentAnimationToHome(data.current.namespace);
         },
-        async after(data){
+        async after(){
             listItemsShow();
-            Cursor()
         },
         from: {
             namespace: [
@@ -72,30 +137,36 @@ barba.init({
 });
 
 
-//home list show effect
-function initHome(){
-    gsap.from('li.list-item a', {y: 170, duration: .8, display: 'block', stagger: 0.2, ease: "power2.out" })
-}
-
 function pageTransitionToPage(arg){
-
-    document.querySelector('.image-container').classList.remove('mouse');
 
     gsap.to('.home-left-container div', {opacity: 0, duration: 0.3});
     gsap.to('li.list-item a', {y: 170, duration: .8, display: 'block', stagger: 0.2, delay: 0.3,  ease: "power2.out" });
+    let images = document.querySelectorAll('.image-container');
 
     switch (arg) {
         case 'about':
             gsap.to('.image-container:nth-child(1)',{opacity: 1, left: 20, top: 20, bottom: 40, width: '40%', maxHeight: '100%', duration: 1, delay: 0.3, ease: "power1.out" } )
+            images[1].style.display = "none";
+            images[2].style.display = "none";
+            images[3].style.display = "none";
             break;
         case 'products':
             gsap.to('.image-container:nth-child(2)',{opacity: 1, left: 20, top: 20, bottom: 40, width: '40%', maxHeight: '100%', duration: 1, delay: 0.3, ease: "power1.out" } )
+            images[0].style.display = "none";
+            images[2].style.display = "none";
+            images[3].style.display = "none";
             break;
         case 'contacts':
             gsap.to('.image-container:nth-child(3)',{opacity: 1, left: 20, top: 20, bottom: 40, width: '40%', maxHeight: '100%', duration: 1, delay: 0.3, ease: "power1.out" } )
+            images[0].style.display = "none";
+            images[1].style.display = "none";
+            images[3].style.display = "none";
             break;
         case 'portfolio':
             gsap.to('.image-container:nth-child(4)',{opacity: 1, left: 20, top: 20, bottom: 40, width: '40%', maxHeight: '100%', duration: 1, delay: 0.3, ease: "power1.out"  } )
+            images[0].style.display = "none";
+            images[1].style.display = "none";
+            images[2].style.display = "none";
             break;
     }
     
@@ -168,91 +239,15 @@ function contentAnimationToHome(arg){
     }
     gsap.from('.home-left-container div', {y: 1000, duration: .8, display: 'block', stagger: 0.2, delay: 0.5, ease: "power2.out"});
 }
-
-//home list show cursor image effects
-function listItemsShow(){
-
-    let items = document.querySelectorAll('.list-item');
-    let links = document.querySelectorAll('a');
-    let images = document.querySelectorAll('.image-container');
-
-    for (let i = 0; i < items.length; i++) {
-        
-        items[i].addEventListener('mouseover', function(){
-
-            switch (this.value) {
-                case 0:
-                    images[0].classList.add("img-reveal");
-                    links[0].style.color = "white";
-                    links[0].classList.add('right-margin');
-                    break;
-                case 1:
-                    images[1].classList.add("img-reveal");
-                    links[1].style.color = "white";
-                    links[1].classList.add('right-margin');
-                    break;
-    
-                case 2:
-                    images[2].classList.add("img-reveal");
-                    links[2].style.color = "white";
-                    links[2].classList.add('right-margin');
-                    break;
-                case 3:
-                    images[3].classList.add("img-reveal");
-                    links[3].style.color = "white";
-                    links[3].classList.add('right-margin');
-                    break;
-            }
-        })
-
-        items[i].addEventListener("mouseleave", function(){
-            switch (this.value) {
-                case 0:
-                    images[0].classList.remove("img-reveal");
-                    links[0].style.color = "black";
-                    links[0].classList.remove('right-margin');
-                    break;
-                case 1:
-                    images[1].classList.remove("img-reveal");
-                    links[1].style.color = "black";
-                    links[1].classList.remove('right-margin');
-                    break;
-                case 2:
-                    images[2].classList.remove("img-reveal");
-                    links[2].style.color = "black";
-                    links[2].classList.remove('right-margin');
-                    break;
-                case 3:
-                    images[3].classList.remove("img-reveal");
-                    links[3].style.color = "black";
-                    links[3].classList.remove('right-margin');
-                    break;
-            }
-        }) 
-    }
+//function used in barba lifecycle to control the effects durations
+function delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
 }
-
-//image cursor effect
-function Cursor(){
-   
-    var $cursor = document.querySelectorAll('.mouse');
-
-    function moveCursor(e) {
-
-        for (let i = 0; i < $cursor.length; i++) {
-            gsap.to($cursor[i], 0.23, {
-                left: e.pageX - 200,
-                top: e.pageY -300,
-                ease: Power4.easOut
-            });
-            
-        }   
-
-    }
-
-    addEventListener('mousemove', moveCursor);
-}
-
 
 
 
